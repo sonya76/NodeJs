b4ab8d9d97caafc053f2a8cd0aca651125543604
Sonia Zaïna CHALAL <soniachalalceppic@gmail.com>
2022-09-13 09:49:52 +0200

    executer un bash-script

---
+++ bash-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+// exécution du fichier bash sans la commande node mais qui exécute du node
+// passer la commande dans la console
+//chmod +x rend le fichier exécutable
+//chmod +x bash-script.js && ./bash-script.js 
+
+console.log("Node.js coucou");
