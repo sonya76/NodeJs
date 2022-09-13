e403a5298bd5a55b94e653d49e2e0e6dd3c194f5
Sonia Zaïna CHALAL <soniachalalceppic@gmail.com>
2022-09-13 10:21:08 +0200

    création server local

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+require('dotenv').config();
+
+//const port = 5000;
+const port = process.env.PORT || 5000;
+//requete : req et response : res
+const server = http.createServer((req, res) => {
+res.statusCode = 200
+res.setHeader('Content-Type', 'text/html')
+res.end('<h1>Welcome to node.js server</h1>')
+});
+
+server.listen(port, ()=>{
+    console.log(`Server lancé à l'adresse http://127.0.0.1:${port}`)
+})
